300bb56813423c36de0da30421812863206a75eb
Jordan~ <jordan.brewer509@gmail.com>
2022-12-29 16:32:11 -0800

    began work on exporting additional data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Photo from './Photo';
+// import Title from './Title';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>NASA Photo of the Day</h1>
       <div className="container">
         <img src={`${Photo()}`} />
+        {/* <h2>{`${Title()}`}</h2> */}
       </div>
     </div>
   );
---
+++ src/Title.js
@@ -0,0 +1,23 @@
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// function Title() {
+
+//     const [title, getTitle] = useState("");
+    
+//         useEffect(() => {
+//             axios.get("https://api.nasa.gov/planetary/apod?", {
+//                 params: {
+//                     api_key: "DEMO_KEY",
+//                 },
+//             })
+//             .then(res => {
+//                 console.log(res);
+//             })
+//             .catch(err => console.log(err))
+// }, []);
+
+// return title;
+// }
+
+// export default Title;
